b4ebd408277456649f8881d6609225e2a7e0b14e
sabamusharraf <sabahussain337@gmail.com>
2024-03-09 21:18:28 +0500

    This is the Assignment of class 3

---
+++ dist/app.js
@@ -0,0 +1,16 @@
+"use strict";
+// Variables
+var myName = "Saba";
+var myName = "Saba Hussain";
+console.log(`My name is ${myName}`);
+let nyName = "Saba";
+nyName = "Saba Hussain";
+console.log(`My name is ${myName}`);
+const firstName = "Saba";
+const lastName = "Hussain";
+console.log(` My name is ${firstName} ${lastName}`);
+let favouriteColor = "Red";
+favouriteColor = "Black";
+console.log(`my favourite color is ${favouriteColor}`);
+const age = 30;
+console.log(`my age is ${age}`);
