04b1a3619bfb76fc9ed19ca8b38a25de5c436b06
sabamusharraf <sabahussain337@gmail.com>
2024-03-15 20:45:44 +0500

    make rootDir and outDir folders

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,14 @@
 "use strict";
 // Variables
-var myName = "Saba";
-var myName = "Saba Hussain";
+var myName = "Saba"; //declare and initiallize
+var myName = "Saba Hussain"; //re-declare
 console.log(`My name is ${myName}`);
 let nyName = "Saba";
-nyName = "Saba Hussain";
+nyName = "Saba Hussain"; //cannot redeclare
 console.log(`My name is ${myName}`);
 const firstName = "Saba";
 const lastName = "Hussain";
-console.log(` My name is ${firstName} ${lastName}`);
+console.log(`My name is ${firstName} ${lastName}`);
 let favouriteColor = "Red";
 favouriteColor = "Black";
 console.log(`my favourite color is ${favouriteColor}`);
